feca691b85dcc6df26649434edc6081b3da124db
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2021-08-08 21:58:52 +0300

    Add files via upload

---
+++ UNTİL PRESS ODD NUM WHİLE/UNTİL PRESS ODD NUM WHİLE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UNTİL_PRESS_ODD_NUM_WHİLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı;
+            Console.Write("Lütfen sayı giriniz : ");
+            sayı=Convert.ToInt32(Console.ReadLine());
+
+            while (sayı % 2 == 0) 
+            {
+                Console.Write("Lütfen sayı giriniz : ");
+                sayı = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Tek sayı girdiniz");
+            Console.ReadKey();
+
+        }
+    }
+}
